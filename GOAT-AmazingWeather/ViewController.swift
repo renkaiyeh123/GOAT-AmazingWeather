3af319c1014087049860b8fb4a1645c6ee1ded59
Andrew Yeh <ayeh93@gmail.com>
2019-11-04 16:51:14 -0800

    Initial Commit

---
+++ GOAT-AmazingWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GOAT-AmazingWeather
+//
+//  Created by Ren-Kai Yeh on 11/4/19.
+//  Copyright © 2019 Sponty, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
